4eb539e044d354456b283ae477e2c4a942164454
Jorge Veliz <jorgeconsalvacion@gmail.com>
2023-05-08 15:43:27 -0500

    arregla menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,8 +1,4 @@
-import React, {
-    FC,
-    useEffect,
-    useRef,
-} from 'react'
+import React, { FC, useEffect, useRef } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
@@ -16,7 +12,7 @@ interface MenuInterface {
 
 export const Menu: FC<MenuInterface> = ({ onOpenSidebar }) => {
     const ref = useRef<HTMLDivElement>(null)
-    
+
     const navigation = useLocation()
 
     const { pathname } = navigation
@@ -38,8 +34,6 @@ export const Menu: FC<MenuInterface> = ({ onOpenSidebar }) => {
     const toggleSidebar = () => {
         onOpenSidebar()
     }
-
-    
 
     useEffect(() => {
         // window.addEventListener('scroll', handleNavigation)
@@ -70,128 +64,60 @@ export const Menu: FC<MenuInterface> = ({ onOpenSidebar }) => {
 
     return (
         <header className="main-header" ref={ref}>
-            {/* <div className="header-upper">
-                <Container>
-                    <div className="clearfix">
-                        <div className="float-start logo-box">
-                            <div className="logo">
-                                <Link to="/">
-                                    <img src={logo} alt="" title="" />
-                                </Link>
-                            </div>
-                        </div>
-
-                        <div className="nav-outer clearfix">
-                            <Navbar
-                                className="main-menu  py-0"
-                                expand="md"
-                                bg="none"
-                                collapseOnSelect={true}
-                            >
-                                <div className="navbar-header">
-                                    <Navbar.Toggle
-                                        aria-controls="navbarScroll"
-                                        onClick={() => console.log('navbar')}
-                                    />
-                                </div>
-                                <Navbar.Collapse
-                                    id="navbarSupportedContent"
-                                    className="clearfix"
-                                >
-                                    <Nav
-                                        as={'ul'}
-                                        className="navigation clearfix"
-                                        navbar={false}
-                                    >
-                                        {menuItems.map((item, idx) => {
-                                            return (
-                                                <li
-                                                    key={idx}
-                                                    className={
-                                                        location ===
-                                                        item.path.split('/')[1]
-                                                            ? 'current'
-                                                            : ''
-                                                    }
-                                                >
-                                                    <Link to={item.path}>
-                                                        {item.name}
-                                                    </Link>
-                                                </li>
-                                            )
-                                        })}
-                                    </Nav>
-                                </Navbar.Collapse>
-                            </Navbar>
-
-                            <div className="outer-box">
-                                <div
-                                    className="nav-toggler"
-                                    onClick={toggleSidebar}
-                                >
-                                    <div className="nav-btn hidden-bar-opener">
-                                        <FontAwesomeIcon icon={faBars} />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Container>
-            </div> */}
-
             <div className="sticky-header">
-                <div className="container clearfix">
-                    <div className="logo float-start">
-                        <Link to='/' className="img-responsive">
+                <Container className="clearfix">
+                    <div className="float-start  logo-box">
+                        <Link to="/" className="img-responsive">
                             <img src={logoSmall} alt="" title="" />
                         </Link>
                     </div>
                     <div className="right-col float-end">
-                        <nav className="main-menu navbar-expand-md">
-                            <button
-                                className="navbar-toggler"
-                                type="button"
-                                data-toggle="collapse"
-                                data-target="#navbarSupportedContent1"
-                                aria-controls="navbarSupportedContent1"
-                                aria-expanded="false"
-                                aria-label="Toggle navigation"
-                            >
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                            </button>
-
-                            <div
-                                className="navbar-collapse collapse clearfix"
-                                id="navbarSupportedContent1"
-                            >
-                                <ul className="navigation clearfix">
-                                    <ul className="navigation clearfix">
-                                        {menuItems.map((item, idx) => {
-                                            return (
-                                                <li
-                                                    key={idx}
-                                                    className={
-                                                        location ===
-                                                        item.path.split('/')[1]
-                                                            ? 'current'
-                                                            : ''
-                                                    }
-                                                >
-                                                    <Link to={item.path}>
-                                                        {item.name}
-                                                    </Link>
-                                                </li>
-                                            )
-                                        })}
-                                    </ul>
-                                </ul>
+                        <Navbar
+                            className="main-menu  py-0"
+                            expand="md"
+                            bg="none"
+                            collapseOnSelect={true}
+                        >
+                            <div className="navbar-header">
+                                <Navbar.Toggle
+                                    aria-controls="navbarScroll"
+                                    onClick={() => console.log('navbar')}
+                                />
                             </div>
-                        </nav>
+                            <Navbar.Collapse
+                                id="navbarSupportedContent"
+                                className="clearfix"
+                            >
+                                <Nav
+                                    as={'ul'}
+                                    className="navigation clearfix"
+                                    navbar={false}
+                                >
+                                    {menuItems.map((item, idx) => {
+                                        return (
+                                            <li
+                                                key={idx}
+                                                className={
+                                                    location ===
+                                                    item.path.split('/')[1]
+                                                        ? 'current'
+                                                        : ''
+                                                }
+                                            >
+                                                <Link to={item.path}>
+                                                    {item.name}
+                                                </Link>
+                                            </li>
+                                        )
+                                    })}
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Navbar>
                     </div>
-                </div>
+                </Container>
             </div>
+
+            
         </header>
     )
 }

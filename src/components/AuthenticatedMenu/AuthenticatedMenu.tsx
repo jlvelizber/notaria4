2b081ef105dafaf165d816fe7d22ccae35b8ee1b
Jorge Veliz <jorgeconsalvacion@gmail.com>
2023-09-12 12:37:39 -0500

    Cambia la forma del menu admin

--- src/components/AuthenticatedMenu/AuthenticatedMenu.tsx
+++ src/components/AuthenticatedMenu/AuthenticatedMenu.tsx
@@ -1,24 +1,18 @@
-import React, { useEffect } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
 import { Nav, NavDropdown, Navbar } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState, onLogout } from '../../store'
-import { useNavigate } from 'react-router'
 
 export const AuthenticatedMenu = () => {
-    const navigate = useNavigate()
     const dispatch = useDispatch()
     const { user } = useSelector((state: RootState) => state.auth)
 
     const logout = () => {
         dispatch(onLogout())
     }
-
-    const gotoRoute = (route: string) => {
-        navigate(route)
-    }
-
     return (
         <Nav as={'ul'} className="ps-5 me-auto auth-menu">
             <Navbar.Collapse id="navbar-dark-example">
@@ -31,13 +25,11 @@ export const AuthenticatedMenu = () => {
                         </>
                     }
                 >
-                    <NavDropdown.Item
-                        onClick={() => gotoRoute('mis-solicitudes')}
-                    >
-                        Mis solicitudes
+                    <NavDropdown.Item >
+                        <Link to={'/mis-solicitudes'}> Mis solicitudes</Link>
                     </NavDropdown.Item>
-                    <NavDropdown.Item onClick={() => gotoRoute('mi-cuenta')}>
-                        Mi perfil
+                    <NavDropdown.Item>
+                        <Link to={'/mi-cuenta'}>Mi perfil </Link>
                     </NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item onClick={logout}>Salir</NavDropdown.Item>

04f55ca8dd10e98a887e6e0dc504e1bc34a5a201
Jorge Veliz <jorgeconsalvacion@gmail.com>
2024-05-08 12:56:15 -0500

    Google tag y analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import TagManager from 'react-gtm-module'
 import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client'
 import { store } from './store'
@@ -8,6 +9,11 @@ import './App.scss'
 import './assets/css/main.css'
 import './assets/css/responsive.css'
 import { Preloader } from './components/Preloader'
+
+const tagManagerArgs = {
+    gtmId: 'GTM-KBXQP3D4',
+}
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
